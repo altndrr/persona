984f9bea23b97cca8ad7e46b9d9172510015d815
Alessandro Conti <alessandro.conti.97@gmail.com>
2020-11-10 16:19:08 +0100

    Add tests for main module

---
+++ src/tests/test_main.py
@@ -0,0 +1,24 @@
+"""Test the main module."""
+
+import os
+import pytest
+
+import src
+
+test_options = [
+    (None, 1),
+    ('-h', 0),
+    ('--help', 0),
+    ('--version', 0),
+]
+
+
+def test_init():
+    """Test the __init__.py file"""
+    assert hasattr(src, '__version__')
+
+
+@pytest.mark.parametrize('options, output', test_options)
+def test_main(options, output):
+    """Test the main file"""
+    assert os.system(f'python -m src {options}') == output
