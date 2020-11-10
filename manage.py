0af56c88f62c8b31a543e28a50bacc8152258af6
Alessandro Conti <alessandro.conti.97@gmail.com>
2020-11-10 12:15:51 +0100

    Add command env export to manage script

--- manage.py
+++ manage.py
@@ -2,11 +2,14 @@
 manage
 
 Usage:
+    manage env export
     manage -h | --help
 
 Options:
     -h --help                   Show this screen.
 """
+
+import os
 
 from docopt import docopt
 
@@ -20,6 +23,18 @@ def main():
         if key in globals() and value:
             command = globals()[key]
             command(options)
+
+
+def env(options):
+    """The env command."""
+    if options['export']:
+        print('Exporting conda packages to `environment.yml`...')
+
+        # Get the name of the search utility given the os.
+        utility = 'findstr' if os.name == 'nt' else 'grep'
+
+        os.system(f'conda env export --from-history --no-builds | \
+                    {utility} -v "prefix" > environment.yml')
 
 
 if __name__ == '__main__':
