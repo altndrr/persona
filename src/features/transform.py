a24685aa77955434bb16ff72717e0cb876b6cfe9
Alessandro Conti <alessandro.conti.97@gmail.com>
2020-11-13 17:58:49 +0100

    Add align_images to features.transform

--- src/features/transform.py
+++ src/features/transform.py
@@ -3,9 +3,48 @@
 import os
 from typing import List, Union
 
+from facenet_pytorch import MTCNN
 from PIL import Image
 from torch import Tensor
 from torchvision.transforms import ToPILImage, ToTensor
+
+mtcnn = MTCNN()
+
+
+def align_images(
+    *images: Union[str, Image.Image], fail_ok=True
+) -> Union[Tensor, List[Tensor]]:
+    """
+    Take image paths and images and try to align them using the MTCNN module
+
+    :param images: paths to images or images
+    :param fail_ok: suppress TypeErrors if alignment is impossible
+    :return: aligned image
+    :raises FileNotFoundError, TypeError:
+    """
+    aligned_images = []
+
+    for image in images:
+        if isinstance(image, str):
+            if not os.path.exists(image) or not os.path.isfile(image):
+                raise FileNotFoundError(f"{image} is an invalid image path")
+
+            image = Image.open(image)
+
+        # Convert to RGB to fix the number of channels to three.
+        image = image.convert("RGB")
+
+        try:
+            aligned_images.append(mtcnn(image))
+        except TypeError:
+            # A face couldn't be detected in the image.
+            if not fail_ok:
+                raise TypeError
+
+    if len(aligned_images) == 1:
+        aligned_images = aligned_images[0]
+
+    return aligned_images
 
 
 def images_to_tensors(*images: Union[str, Image.Image]) -> Union[Tensor, List[Tensor]]:
--- src/tests/features/test_transform.py
+++ src/tests/features/test_transform.py
@@ -3,11 +3,34 @@
 import pytest
 import torch
 from PIL import Image
+from torchvision.transforms import ToPILImage
 
 from src.data.agedb import AgeDB
 from src.features import transform
 
 age = AgeDB()
+
+
+@pytest.mark.skipif(not AgeDB.is_available(), reason="requires the agedb dataset")
+def test_align_images():
+    """Test the align_images function"""
+    image_path_1 = age.get_image(index=0)
+    image_path_2 = age.get_image(index=1)
+
+    generated_image = ToPILImage()(torch.ones(3, 160, 160))
+
+    with pytest.raises(FileNotFoundError):
+        transform.align_images("")
+
+    with pytest.raises(TypeError):
+        transform.align_images(generated_image, fail_ok=False)
+
+    assert len(transform.align_images(generated_image, fail_ok=True)) == 0
+    assert isinstance(transform.align_images(image_path_1), torch.Tensor)
+    assert isinstance(transform.align_images(image_path_1, image_path_2), list)
+    assert len(transform.align_images(image_path_1, image_path_2)) == 2
+    assert isinstance(transform.align_images(), list)
+    assert len(transform.align_images()) == 0
 
 
 @pytest.mark.skipif(not AgeDB.is_available(), reason="requires the agedb dataset")
